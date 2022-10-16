f8a2707c80f4d8d08cb6e7ddbaa7c6c23cf79191
“Oscar <lucmaxzarate@gmail.com>
2022-10-16 01:22:58 -0300

    modelo pedido y plato

--- Laboratorio/Rotiseria/apps/Pedido/models.py
+++ Laboratorio/Rotiseria/apps/Pedido/models.py
@@ -1,3 +1,36 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
+
+class Pedidos(models.model):
+    cod_pedido = models.AutoField(PrimaryKey=True)
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    #usuario = models.ForeignKey(Usuario,blank=False,null=False,on_delete=models.CASCADE)
+    opcion = [
+        ('Envio','envio'),
+        ('Retiro','retiro'),
+
+    ]
+    modalidadentrega = models.CharField(choices=opcion)
+    hora_entrega_desde = models.DateTimeField()
+    estado_entrega = models.CharField(max_length=300)
+    hora_entrega_hasta = models.DateTimeField()
+
+class platos(models.model):
+    codigo_plato = models.AutoField(PrimaryKey=True)
+    tipo_platos = [
+        ('Entrada','entrada'),
+        ('Plato Principal','plato Principal','principal')
+        ('postre','Postre')
+    ]
+    tipo_plato = models.CharField(choices=tipo_platos)
+    especialidadades = [
+        ('Normal','normal'),
+        ('Vegetariano','vegetariano')
+        ('celiaco','Celiaco')
+        ('diabetico','Diabetico')
+
+    ]
+    especialidad = models.CharField(choices=especialidadades)
+    pedido = models.ManyToManyField(Pedidos)
