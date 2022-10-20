d0f14da737b5465d2d926f816fac5f9e310a70f9
Daniel_juarez97 <juarezz.daniel97@gmail.com>
2022-10-20 16:14:03 -0300

    Modelos de Apps Usuarios completa

--- Laboratorio/Rotiseria/apps/Usuario/admin.py
+++ Laboratorio/Rotiseria/apps/Usuario/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from apps.Usuario.models import Domicilio, Persona, ZonaDomicilio, cadete
+from apps.Usuario.models import Domicilio, Persona, Telefono, ZonaDomicilio, cadete
 
 # Register your models here.
 admin.site.register(Persona)
 admin.site.register(Domicilio)
 admin.site.register(ZonaDomicilio)
 admin.site.register(cadete)
+admin.site.register(Telefono)
--- Laboratorio/Rotiseria/apps/Usuario/models.py
+++ Laboratorio/Rotiseria/apps/Usuario/models.py
@@ -1,5 +1,9 @@
+from cgitb import text
+from email.policy import default
 from enum import unique
+from random import choices
 from re import S
+from secrets import choice
 from tkinter import CASCADE
 from django.db import models
 
@@ -9,6 +13,9 @@ class ZonaDomicilio(models.Model):
     cod_zona = models.AutoField(primary_key=True)
     descripcion_zona = models.CharField(max_length=200,blank= True)
 
+    def __str__(self):
+        texto = "{0}"
+        return texto.format(self.descripcion_zona)
 
 
 class Domicilio(models.Model):
@@ -18,27 +25,40 @@ class Domicilio(models.Model):
     nombre_barrio = models.CharField(max_length=200,blank= True)
     zona = models.OneToOneField(ZonaDomicilio, on_delete=models.CASCADE)
 
+    def __str__(self):
+        texto = "Barrio: {0} - Calle: {1} - Número: {2} - Zona: {3}"
+        return texto.format(self.nombre_barrio, self.nombre_calle,self.numero_calle,self.zona)
 
+class Telefono(models.Model):
+    telefono_op=[('fijo','Fijo'), ('celular','Celular')]
+    tipo_telefono = models.CharField(max_length=10, choices=telefono_op)
+    numero = models.CharField(max_length=10,blank=True)
+    
+    def __str__(self):
+        texto = "{0} / {1}"
+        return texto.format(self.numero,self.tipo_telefono)
 
 class Persona(models.Model):
-    cuil = models.CharField(max_length=10, unique=True)
+    cuil = models.CharField(max_length=11, unique=True)
     nombre = models.CharField(max_length=200, blank= True)
     apellido = models.CharField(max_length=200, blank= True)
     fecha_nacimiento = models.DateField(blank= True)
     email = models.CharField(max_length=300,blank= True)
     domicilio = models.OneToOneField(Domicilio, on_delete=models.CASCADE)
+    telefono = models.ForeignKey(Telefono, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        texto = "{0} - {1} - {2} - {3} - {4} - {5}"
+        return texto.format(self.cuil, self.apellido, self.nombre, self.fecha_nacimiento, self.domicilio, self.email)
 
 
 class cadete(Persona):
-    
     fecha_vigencia_carnet = models.DateField(blank=True)
     numero_patente = models.CharField(max_length=7,blank=True)
     fecha_ingreso = models.DateField(blank=True)
+    
+    def __str__(self):
+        texto = "{0} - Ingreso laboral: {1} - Vigencia carnet: {2} - Patente: {3} - Zona: {4}"
+        return texto.format(self.cuil, self.fecha_ingreso, self.fecha_vigencia_carnet, self.numero_patente, self.domicilio.zona)
 
-""" 
 
-class Usuario(Persona):
-    #nombreusuario = models.CharsFiled(max_length=300)
-    #contraseña = models.CharsField(max_length=300)
-    #tal vez esto lo determine en admin con los roles
-"""
