24e9dce7d13df3b85a17071305ff8b8a6c0bdb4c
“Oscar <lucmaxzarate@gmail.com>
2022-11-04 13:16:14 -0300

    actualizar etado cadete y hora entrega desde

--- Laboratorio/Rotiseria/apps/Pedido/url.py
+++ Laboratorio/Rotiseria/apps/Pedido/url.py
@@ -1,9 +1,11 @@
 from django import views
 from django.urls import path
 from django.urls.resolvers import URLPattern
+from apps.Pedido.views import lista_pedidosadmin
+from apps.Pedido.views import detalle_pedidoadmin, pedidoadmin_edit
 from apps.Pedido.views import detalle_pedido
 from apps.Pedido.views import pedido_edit
-from apps.Pedido.views import CarritoPedidoCliente,agregar_plato_carrito, editar_plato, eliminar_plato_carrito, limpiar_carrito, listarCategoriaPlato,menu_delete,lista_menus,creacion_pedido,lista_pedidos_cadetes,lista_pedidos,menu_detalle,creacion_menu, procesar_compra, promociones, restar_plato_carrito
+from apps.Pedido.views import CarritoPedidoCliente,agregar_plato_carrito, eliminar_plato_carrito, limpiar_carrito, listarCategoriaPlato,menu_delete,lista_menus,creacion_pedido,lista_pedidos_cadetes,lista_pedidos,menu_detalle,creacion_menu, procesar_compra, promociones, restar_plato_carrito
 
 
 app_name='Pedido'
@@ -18,9 +20,12 @@ path('ListaDepedidos',lista_pedidos, name="ListaDepedidos"),
 path('listapedidoscadetes',lista_pedidos_cadetes, name="listapedidoscadetes"),
 path('listademenus',lista_menus, name="listademenus"),
 path('delete/',menu_delete, name="menu_delete"),
-path('editarPlato/<int:pk>',editar_plato, name='editar_plato'),
+# path('editarPlato/<int:pk>',editar_plato, name='editar_plato'),
 path('edit/<int:pk>',pedido_edit, name='edit_pedido'),
- path('view//<int:pk>',detalle_pedido, name='detalle_pedido'),
+path('view/<int:pk>',detalle_pedido, name='detalle_pedido'),
+path('editarpedido/<int:pk>',pedidoadmin_edit, name='pedidoadmin_edit'),
+path('viewpedido/<int:pk>',detalle_pedidoadmin, name='detalle_pedidoadmin'),
+path('listapedidosadmin',lista_pedidosadmin, name="lista_pedidosadmin"),
 
 #carrito
 path('CarritoPedidoCliente',CarritoPedidoCliente, name='CarritoPedidoCliente'),
